68c30a4a8564e2677aed6210603b8c0cc6f68ef0
EC2 Default User <ec2-user@ip-172-31-16-98.us-west-2.compute.internal>
2016-05-25 07:41:50 +0000

    Added more feed resources

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var xmlParser = require('xml2json');
 
 
 
-app.get('/', function (req, res) {
-  
+app.get('/', function(req, res) {
 
-res.send('API Home');
+
+    res.send('API Home');
 
 
 });
@@ -30,7 +30,7 @@ res.send('API Home');
 //Start code
 
 app.get('/<API_ROUTE>', function (req, res) {
-  
+
 
 var http=require('http');
 
@@ -44,11 +44,11 @@ var http=require('http');
          });
 
         data.on('end', function () {
-             
+
              //got the data, now parse
 
              var xml = str;
-var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default 
+var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
 console.log(json);
 res.send(json);
          });
@@ -68,87 +68,326 @@ res.send(json);
 
 
 
+//Start code for Fitness API
 
-app.get('/nutritionAPI', function (req, res) {
-  
+app.get('/fitnessAPI', function(req, res) {
 
-var http=require('http');
 
-   http.get('http://www.health.com/nutrition/feed', function(data){
+    var http = require('http');
+
+    http.get('http://www.health.com/fitness/feed', function(data) {
         var str = '';
-        console.log('Response is '+data.statusCode);
+      //  console.log('Response is ' + data.statusCode);
 
-        data.on('data', function (chunk) {
-              //console.log('BODY: ' + chunk);
-               str += chunk;
-         });
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
 
-        data.on('end', function () {
-             
-             //got the data, now parse
+        data.on('end', function() {
 
-             var xml = str;
-var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default 
-console.log(json);
-res.send(json);
-         });
+            //got the data, now parse
 
-  });
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+          //  console.log(json);
+            res.send(json);
+        });
+
+    });
 
 
 
 });
+//------END CODE for Fitness API
 
 
 
+//Start code for Pets API
+app.get('/petsAPI', function(req, res) {
 
 
+    var http = require('http');
 
-
-
-app.get('/foodAPI', function (req, res) {
-  
-
-var http=require('http');
-
-   http.get('http://www.health.com/food/feed', function(data){
+    http.get('http://www.health.com/pets/feed', function(data) {
         var str = '';
-        console.log('Response is '+data.statusCode);
+      //  console.log('Response is ' + data.statusCode);
 
-        data.on('data', function (chunk) {
-              //console.log('BODY: ' + chunk);
-               str += chunk;
-         });
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
 
-        data.on('end', function () {
-             
-             //got the data, now parse
+        data.on('end', function() {
 
-             var xml = str;
-var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default 
-console.log(json);
-res.send(json);
-         });
+            //got the data, now parse
 
-  });
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+          //  console.log(json);
+            res.send(json);
+        });
+
+    });
+
+
+
+});
+//------END CODE for Pets API
+
+
+
+
+
+app.get('/nutritionAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/nutrition/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+          //  console.log(json);
+            res.send(json);
+        });
+
+    });
 
 
 
 });
 
 
+app.get('/foodAPI', function(req, res) {
 
 
+    var http = require('http');
+
+    http.get('http://www.health.com/food/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+    });
 
 
-
-
-
-
-
-
-
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
 });
+
+// Family API
+
+
+app.get('/familyAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/family/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+    });
+
+
+});
+
+
+//Family API Ends
+
+// Mind and Body API
+
+
+app.get('/mindAndBodyAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/mind-body/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+	data.on('error' , function(){
+console.log("There was an error");
+});
+
+    });
+
+
+});
+
+
+//Mind and Body API Ends
+
+
+// Home API
+
+
+app.get('/homeAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/home/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+    });
+
+
+});
+
+
+//Home API Ends
+
+
+
+
+// Recipes API
+
+
+app.get('/recipesAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/recipes/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+    });
+
+
+});
+
+
+//Recipes API Ends
+
+
+// Weight Loss API
+
+
+app.get('/weightLossAPI', function(req, res) {
+
+
+    var http = require('http');
+
+    http.get('http://www.health.com/weight-loss/feed', function(data) {
+        var str = '';
+      //  console.log('Response is ' + data.statusCode);
+
+        data.on('data', function(chunk) {
+            //console.log('BODY: ' + chunk);
+            str += chunk;
+        });
+
+        data.on('end', function() {
+
+            //got the data, now parse
+
+            var xml = str;
+            var json = xmlParser.toJson(xml); //returns a string containing the JSON structure by default
+      //      console.log(json);
+            res.send(json);
+        });
+
+    });
+
+
+});
+
+
+//Weight Loss API Ends
+
+
+
+
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
+
